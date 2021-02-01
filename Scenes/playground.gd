35194d917f4e0e0ca93317add21e7d76b1a4ee7c
VcelistMC <60217976+VcelistMC@users.noreply.github.com>
2021-02-02 00:07:03 +0200

    completed tic-tac-toe functionality

--- Scenes/playground.gd
+++ Scenes/playground.gd
@@ -6,25 +6,48 @@ extends CenterContainer
 # var b = "text"
 var turn = 1
 var res
+var buttons
+var grid = [
+	"", "", "", 
+	"", "", "", 
+	"", "", ""
+]
+var curr
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	buttons = $GridContainer.get_children()
+	print(grid)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
-
-
-func _on_TextureButton_pressed():
-	if turn == 1:
-		res = "../assests/x-button.png"
-		print("x")
-	else:
-		res = "../assests/o-button.png"
-		print("o")
-	$GridContainer/CenterContainer/TextureButton.texture_normal = load(res)
-	turn = -turn
+#	pas
 	
-# texture disappers when pressed, even tho it loads nicely using the inspector
+
+func _on_TextureButton_pressed(ind):
+	if turn == 1:
+		res = "res://assests/x-button.png"
+		curr = "X"
+	else:
+		res = "res://assests/o-button.png"
+		curr = "O"
+	buttons[ind-1].texture_normal = load(res)
+	buttons[ind-1].disabled = true
+	turn = -turn
+	grid[ind-1] = curr
+	if check_winner(curr): 
+		$Label.text = curr + " WON"
+		for button in buttons:
+			button.disabled = true
+
+func check_winner(player):
+	for i in range(3):
+		var cell = 3*i
+		if grid[cell] == grid[cell+1] and curr == grid[cell+2]: return true #check rows
+		elif grid[i] == grid[i+3] and curr == grid[i+6]: return true #check cols
+	
+	if grid[0] == grid[4] and curr == grid[8]: return true
+	elif grid[2] == grid[4] and curr == grid[6]: return true
+	else: return false
+	
