13809636f58e7d2eb741aacb01e027b8ba568465
VcelistMC <60217976+VcelistMC@users.noreply.github.com>
2021-02-06 23:55:44 +0200

    minimax is now working correctly, added initial visualization logic

--- Scenes/playground.gd
+++ Scenes/playground.gd
@@ -9,32 +9,58 @@ var grid = [
 	"", "", "", 
 	"", "", ""
 ]
-var player = "O"
-var opponent = "X"
+var player = "X"
+var opponent = "O"
+
+var x_texture = load("res://assests/x-button.png")
+var o_texture = load("res://assests/o-button.png")
+var empty = load("res://assests/download.png")
+var moves
 
 
 func _ready():
 	buttons = $GridContainer.get_children()
-	var move = findBestMove(grid)
-	print("best move for x is" + str(move+1))
-	print("yeeet")
+	
+func wait(time):
+	var t = Timer.new()
+	t.set_wait_time(time)
+	t.set_one_shot(true)
+	$".".add_child(t)
+	t.start()
+	yield(t, "timeout")
 	
 
 func _on_TextureButton_pressed(ind):
-	if turn == 1:
-		res = "res://assests/x-button.png"
-		curr = "X"
-		var move = findBestMove(grid)
-		print("best move for x is" + str(move+1))
-	else:
-		res = "res://assests/o-button.png"
-		curr = "O"
-	buttons[ind-1].texture_normal = load(res)
+	buttons[ind-1].texture_normal = o_texture
 	buttons[ind-1].disabled = true
 	turn = -turn
-	grid[ind-1] = curr
-	if isWinner(curr): 
-		$Label.text = curr + " WON"
+	grid[ind-1] = "O"
+	if isWinner("O"): 
+		$Label.text = "O WON"
+		for button in buttons:
+			button.disabled = true
+	
+	var move = findBestMove(grid)
+	for i in moves:
+		if i >= 0:
+			buttons[i].texture_normal = x_texture if turn == 1 else o_texture
+			turn = -turn
+		else:
+			buttons[abs(i)].texture_normal = empty
+		
+		var t = Timer.new()
+		t.set_wait_time(0.01)
+		t.set_one_shot(true)
+		self.add_child(t)
+		t.start()
+		yield(t, "timeout")
+			
+	buttons[move].texture_normal = x_texture
+	buttons[move].disabled = true
+	grid[move] = "X"
+	
+	if isWinner("X"): 
+		$Label.text = "X WON"
 		for button in buttons:
 			button.disabled = true
 	
@@ -42,11 +68,16 @@ func _on_TextureButton_pressed(ind):
 func isWinner(potential_winner):
 	for i in range(3):
 		var cell = 3*i
-		if grid[cell] == grid[cell+1] and potential_winner == grid[cell+2]: return true #check rows
-		elif grid[i] == grid[i+3] and potential_winner == grid[i+6]: return true #check cols
+		if grid[cell] == grid[cell+1] and grid[cell+1] == grid[cell+2]: 
+			return (grid[cell] == potential_winner) #check rows
+		elif grid[i] == grid[i+3] and grid[i+3] == grid[i+6]: 
+			return (grid[i] == potential_winner) #check cols
 	
-	if grid[0] == grid[4] and potential_winner == grid[8]: return true
-	elif grid[2] == grid[4] and potential_winner == grid[6]: return true
+	if grid[0] == grid[4] and grid[4] == grid[8]: 
+		return (grid[0] == potential_winner)
+	
+	elif grid[2] == grid[4] and grid[4] == grid[6]: 
+		return (grid[2] == potential_winner)
 	else: return false
 	
 func evaluate():
@@ -63,27 +94,40 @@ func minimax(board, depth, maxPlayer):
 		var bestMove = -10
 		for cell in range(9):
 			if board[cell] == "":
+				moves.append(cell)
 				board[cell] = player
+				buttons[cell].texture_normal = o_texture if player == "O" else x_texture
 				bestMove = max(bestMove, minimax(board, depth+1, !maxPlayer))
 				board[cell] = ""
+				moves.append(-cell)
+				buttons[cell].texture_normal = empty
 		return bestMove
 	else:
 		var bestMove = 10
 		for cell in range(9):
 			if board[cell] == "":
+				moves.append(cell)
 				board[cell] = opponent
+				buttons[cell].texture_normal = o_texture if player == "O" else x_texture
 				bestMove = min(bestMove, minimax(board, depth+1, !maxPlayer))
 				board[cell] = ""
+				moves.append(-cell)
+				buttons[cell].texture_normal = empty
 		return bestMove
 
 func findBestMove(board):
 	var bestVal = -10000
 	var bestmove = -1
+	moves = []
 	for cell in range(9):
 		if board[cell] == "":
+			moves.append(cell)
 			board[cell] = player
+			buttons[cell].texture_normal = o_texture if player == "O" else x_texture
 			var val = minimax(board, 0, false)
 			board[cell] = ""
+			moves.append(-cell)
+			buttons[cell].texture_normal = empty
 			if val > bestVal:
 				bestVal = val
 				bestmove = cell
